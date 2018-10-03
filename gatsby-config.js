0ee8bef0b5056c0026263bd279ae9fa61cdc21ba
Kristjan Kuusik <kristjn.k@gmail.com>
2018-10-03 10:14:56 +0300

    test

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
     'gatsby-transformer-json',
+    'gatsby-transformer-remark',
     {
       resolve: 'gatsby-source-filesystem',
       options: {
