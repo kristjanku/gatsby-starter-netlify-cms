c43d2fa70224860e39a303f14e2ab18a4ec24aa2
Kristjan Kuusik <kristjn.k@gmail.com>
2018-10-04 11:02:55 +0300

    test

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,7 +7,6 @@ exports.createPages = ({ boundActionCreators, graphql }) => {
 
   return graphql(`
     {
-      // allMarkdownRemark(limit: 1000)
       allPostsJson(limit: 1000) {
         edges {
           node {
